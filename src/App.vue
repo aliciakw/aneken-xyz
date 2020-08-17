e9f08111e9bfae8784ff15ff31ff2ce701873b24
Alicia Willett <alicia@sanctuary.computer>
2020-08-17 18:40:07 -0400

    200817 typography

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="app">
-    <p>Hello Vue</p>
+    <h1 class="title handwriting text-center color-shark-blue mt1">
+      Aneken River
+    </h1>
+    <PageContainer slug="home" />
+    <p class="mt2 text-center">Thanks for visiting!</p>
   </div>
 </template>
 
 <script>
+import PageContainer from './components/PageContainer.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    PageContainer
+  }
 }
 </script>
+
+<style>
+body {
+  background-color: #131921;
+  color: #f2f2f2;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+</style>
