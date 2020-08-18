eff2a2a777eeca38e2752be1fa07fd32cdff19b2
Alicia Willett <alicia@sanctuary.computer>
2020-08-18 08:40:51 -0400

    200817 retrieve nested block list

---
+++ src/components/ImageBlock.vue
@@ -0,0 +1,8 @@
+<template>
+  <p>ImageBlock!</p>
+</template>
+<script>
+export default {
+  name: 'ImageBlock',
+}
+</script>
--- src/components/PageContainer.vue
+++ src/components/PageContainer.vue
@@ -1,16 +1,38 @@
 <template>
-  <p class="heading amatic-sc text-center">PageContainer :: {{ slug }}</p>
+  <div v-if="blocks">
+    <div v-for="block in blocks" v-bind:key="block.id">
+      <ImageBlock v-if="block.type === 'imageblock'" />
+    </div>
+  </div>
+  <p v-else class="heading amatic-sc text-center">PageContainer :: {{ slug }}</p>
 </template>
 <script>
+import ImageBlock from './ImageBlock';
 const graphQuery = `{
   page {
-    ...pageFields
+    title
+    block_links {
+      block {
+        ...on imageblock {
+          title
+          image
+        }
+      }
+    }
   }
 }`;
 export default {
   name: 'PageContainer',
+  components: {
+    ImageBlock
+  },
   props: {
     slug: String,
+  },
+  data() {
+    return {
+      blocks: null
+    };
   },
   mounted() {
     this.loadBlocks();
@@ -19,8 +41,16 @@ export default {
     loadBlocks: function() {
       this.$prismic.client.getByUID('page', this.slug, { 'graphQuery': graphQuery })
         .then((document) => {
-          if (document && document.data && Array.isArray(document.data.blocks)) {
-            console.log(document.data);
+          console.log(document.data);
+          if (document && document.data && Array.isArray(document.data.block_links)) {
+            const blocks = document.data.block_links.reduce((acc, blockFields) => {
+              if (blockFields.block.type && !blockFields.block.isBroken) {
+                console.log('>', blockFields.block.data)
+                acc.push(blockFields.block);
+              }
+              return acc;
+            }, []);
+            this.blocks = blocks;
           }
         });
     }
