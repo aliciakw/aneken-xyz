8f1aa0e63718e0ad36901c326d2079911099d819
Alicia Willett <alicia@sanctuary.computer>
2020-08-18 11:57:11 -0400

    200818 Footer, global settings request

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,59 @@
 <template>
-  <div id="app">
-    <h1 class="title handwriting text-center color-shark-blue mt1">
+  <div id="app" class="bg-black">
+    <h1 class="title handwriting text-center color-shark-blue">
       Aneken River
     </h1>
     <PageContainer slug="home" />
+    <FooterComponent
+      v-bind:bandcampUrl="bandcampUrl"
+      v-bind:instagramUrl="instagramUrl"
+      v-bind:spotifyUrl="spotifyUrl"
+      v-bind:youtubeUrl="youtubeUrl"
+    />
   </div>
 </template>
 
 <script>
-import PageContainer from './components/PageContainer.vue'
+import PageContainer from './containers/PageContainer.vue';
+import FooterComponent from './components/FooterComponent.vue';
 export default {
   name: 'app',
   components: {
-    PageContainer
+    PageContainer,
+    FooterComponent,
+  },
+  data() {
+    return {
+      contactEmail: '',
+      bandcampUrl: '',
+      instagramUrl: '',
+      spotifyUrl: '',
+      youtubeUrl: '',
+    }
+  },
+  mounted() {
+    this.fetchGlobalSettings();
+  },
+  methods: {
+    fetchGlobalSettings: function() {
+      this.$prismic.client.getByUID('global_settings', 'global_settings')
+        .then((document) => {
+          if (document && document.data) {
+            this.contactEmail = document.data.contact_email || '';
+            this.bandcampUrl = document.data.bandcamp_url || '';
+            this.instagramUrl = document.data.instagram_url || '';
+            this.spotifyUrl = document.data.spotify_url || '';
+            this.youtubeUrl = document.data.youtube_url || '';
+          }
+        });
+    }
   }
 }
 </script>
 
 <style>
 body {
-  background-color: #131921;
+  background: #6d9cd0; /* .bg-shark-blue */
   color: #f2f2f2;
   font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
--- src/components/ImageBlock.vue
+++ src/components/ImageBlock.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-col align-center w100">
+  <div class="flex flex-col align-center p1">
     <ImageLoader v-bind:src="data.image.url" v-bind:alt="data.image.alt" />
     <p class="copy mt1 mb2">{{data.image.alt}}</p>
   </div>
 </template>
 <script>
-import ImageLoader from './ImageLoader';
+import ImageLoader from '../containers/ImageLoader';
 export default {
   name: 'ImageBlock',
   components: {
